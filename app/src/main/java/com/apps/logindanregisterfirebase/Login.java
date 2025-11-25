c1641936b301085dcae4a40cde65f2aa83dcf27a
ayat7779 <ayat7779@yahoo.com>
2025-11-25 12:29:42 +0700

    Komit Pertama

---
+++ app/src/main/java/com/apps/logindanregisterfirebase/Login.java
@@ -0,0 +1,90 @@
+package com.apps.logindanregisterfirebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Login extends AppCompatActivity {
+
+    private EditText etUsername, etPassword;
+    private Button btnLogin, btnRegister;
+    private DatabaseReference database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register = new Intent(getApplicationContext(), Register.class);
+                startActivity(register);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                database = FirebaseDatabase.getInstance().getReference("users");
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Username atau password Salah!!!", Toast.LENGTH_SHORT).show();
+                }else{
+                    database.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.child(username).exists()){
+                                if(snapshot.child(username).child("password").getValue(String.class).equals(password)){
+                                    Toast.makeText(getApplicationContext(), "Login Berhasil!!!", Toast.LENGTH_SHORT).show();
+                                    Intent masuk = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(masuk);
+                                }else{
+                                    Toast.makeText(getApplicationContext(), "Password Salah!!!", Toast.LENGTH_SHORT).show();
+                                }
+                            }else{
+                                Toast.makeText(getApplicationContext(), "Data Belum Terdaftar!!!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/apps/logindanregisterfirebase/Register.java
@@ -0,0 +1,76 @@
+package com.apps.logindanregisterfirebase;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Register extends AppCompatActivity {
+
+    private EditText etUsername, etPassword, etEmail;
+    private Button btnRegister;
+    private DatabaseReference database;
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        database = FirebaseDatabase.getInstance().getReferenceFromUrl("https://login-dan-register-fireb-bc103-default-rtdb.firebaseio.com/");
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String email = etEmail.getText().toString();
+
+                if(username.isEmpty() || password.isEmpty() || email.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Ada data yang masih kosong!!!", Toast.LENGTH_SHORT).show();
+                }else{
+                    database = FirebaseDatabase.getInstance().getReference("users");
+                    database.child(username).child("username").setValue(username);
+                    database.child(username).child("email").setValue(email);
+                    database.child(username).child("password").setValue(password);
+
+                    Toast.makeText(getApplicationContext(), "Register Berhasil!!!", Toast.LENGTH_SHORT).show();
+                    Intent register = new Intent(getApplicationContext(), Login.class);
+                    startActivity(register);
+                }
+            }
+        });
+
+
+
+
+
+    }
+}
